4589516a9071670d367cbe149c622214504db3ef
jgzuishuai <jgzuishuai0@163.com>
2022-09-29 17:05:00 +0800

    Initial commit

---
+++ src/com/test/TestDemo.java
@@ -0,0 +1,11 @@
+package com.test;
+
+/**
+ * @author jgzuishuai
+ * @create 2022-09-29-17:04
+ */
+public class TestDemo {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
